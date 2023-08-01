e92f36c7f5a7d7cfcba8079acce37522e694afaf
Šimon Oravec <simoonoravec@gmail.com>
2023-08-01 23:12:11 +0200

    Create ipset-blacklist.sh

---
+++ ipset-blacklist.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Root user check
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+#Create or flush sets
+ipset -L blacklist >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+   ipset create blacklist hash:net family inet hashsize 16384 maxelem 65536
+else
+   ipset flush blacklist
+fi
+
+#Update sets
+COUNTRIES=('cn' 'ru' 'by' 'in')
+
+for i in "${COUNTRIES[@]}"; do
+ for IP in $(wget --no-check-certificate -q -O - https://www.ipdeny.com/ipblocks/data/countries/${i}.zone)
+ do
+  ipset add "blacklist" $IP
+ done
+done
+
+#Save sets
+mkdir -p /etc/ipset
+ipset save > /etc/ipset/rules
